5e44a308ea646bacefa6ca274ac1190535561a1c
Darkunralle <darkunralle@gmail.com>
2020-11-25 10:57:00 +0100

    V0.1 collision
    Version basique de la collision

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // Création du plateau
-var plateau = $('<div class = "plateau" style="background-color: #90EE90; height: 800px; width: 800px;position:absolute;"></div>');
+var plateau = $('<div class = "plateau" style="background-color: #90EE90; height: 200px; width: 200px;position:absolute;"></div>');
 // Création de la pastille link
 var link = $('<div class ="link" style="color: blue; background-color: blue; height: 20px; width: 20px;position:absolute; border-radius: 50%;"></div>');
 // Création du vase
 var vase = $('<div class ="vase" style="color: brown; background-color: brown; height: 20px; width: 20px; position:absolute;"></div>');
 
 // Initialisation variable de déplacement
-var lx = 30;
-var ly = 30;
+var lx = 20;
+var ly = 20;
 
 $("body").append(plateau);
 $(plateau).append(link);
@@ -27,15 +27,41 @@ plateau.css({
 
 // Placement initiale du vase
 vase.css({
-    "left": "310px",
-    "top": "310px",
+    "left": "100px",
+    "top": "100px"
 })
 
 
 $("body").keypress(pressfunction);
 
-function pressfunction(event){
+// Tableau du niveau (mur et case vide)
+var tab = 
+[
+    [1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,1,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,1,1,1]
+];
 
+
+// Fonction détection de collision
+function collision(tab){
+    var xt = lx;
+    var yt = ly;
+    if(tab[xt/20][yt/20] == 1){
+        return true;
+    }
+    else{return false;}
+}
+
+function pressfunction(event){
+    var condition = false;
     // /!\ Faire des animations /!\
 
     // Recupere le code de la touche
@@ -43,30 +69,39 @@ function pressfunction(event){
 
     // Identifie le code ASCII de la touche z
     if(key==122){
-        if(ly >= 60){
-            ly -= 20;
-            key=0;}}
+        ly -= 20;
+        key=0;
+        condition = collision(tab);
+        if(condition == true){
+            ly+=20;}}
 
     // Identifie le code ASCII de la touche s
     if(key==115){
-        if(ly <= 740){
-            ly += 20;
-            key=0;}}
+        ly += 20;
+        key=0;
+        condition = collision(tab);
+        if(condition == true){
+            ly-=20;}}
     // Identifie le code ASCII de la touche d
     if(key==100){
-        if(lx <= 740){
-            lx += 20;
-            key=0;}}
+        lx += 20;
+        key=0;
+        condition = collision(tab);
+        if(condition == true){
+            lx-=20;}}
 
     // Identifie le code ASCII de la touche q
     if(key==113){
-        if(lx >= 60){
-            lx -= 20;
-            key=0;}}
+        lx -= 20;
+        key=0;
+        condition = collision(tab);
+        if(condition == true){
+            lx += 20;
+        }}
 
     // Rectifie le placement de la pastille link selon la touche
     link.css({
         "left": lx + "px",
-        "top": ly + "px",
+        "top": ly + "px"
     });
 }
